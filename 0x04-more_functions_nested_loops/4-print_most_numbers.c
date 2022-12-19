d08775fb06df2edf355062f365c5f0e404b20160
Jaysinmars <jaysinmars@gmail.com>
2022-12-19 03:51:19 -0800

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints
+ *owned by jaysin
+ *Return: void
+ **/
+
+
+
+void print_most_numbers(void)
+
+{
+
+	char c;
+
+
+
+	for (c = '0'; c <= '9'; c++)
+
+	{
+
+	if (!(c == '2' || c == '4'))
+
+	_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+}
